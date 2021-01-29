a1e520550579d9b6e018573c9672d0a18d61c072
Romulo S. Ramos <rsromulo@msn.com>
2021-01-28 23:38:04 -0300

    Aula 3

--- pages/index.js
+++ pages/index.js
@@ -1,30 +1,14 @@
 import React, { useState } from 'react';
-import styled from 'styled-components';
 import { useRouter } from 'next/router';
 import db from '../db.json';
 import Widget from '../src/components/Widget';
 import QuizBackground from '../src/components/QuizBackground';
+import QuizContainer from '../src/components/QuizContainer';
 import Footer from '../src/components/Footer';
 import GitHubCorner from '../src/components/GitHubCorner';
 import QuizLogo from '../src/components/QuizLogo';
-
-// const BackgroundImage = styled.div`
-//  background-image: url(${db.bg});
-//  flex: 1;
-//  background-size: cover;
-//  background-position: center;
-// `;
-
-const QuizContainer = styled.div`
-width: 100%;
-max-width: 350px;
-padding-top: 45px;
-margin: auto 10%;
-@media screen and (max-width: 500px) {
-  margin: auto;
-  padding: 15px;
-}
-`;
+import Input from '../src/components/Input';
+import Button from '../src/components/Button';
 
 export default function Home() {
   const router = useRouter();
@@ -39,22 +23,24 @@ export default function Home() {
           </Widget.Header>
           <Widget.Content>
             <p>{db.description}</p>
-            <form onSubmit={function (infosDoEvento) {
+            <form onSubmit={(infosDoEvento) => {
               infosDoEvento.preventDefault();
               router.push(`/quiz?name=${name}`);
               // router manda para a proxima pagina
             }}
             >
-              <input
+              <Input
+                name="nomeDoUsuario"
                 onChange={(infosDoEvento) => setName(infosDoEvento.target.value)}
                 placeholder="Diz ai seu Nome"
+                value={name}
               />
-              <button
+              <Button
                 type="submit"
                 disabled={name.length === 0}
               >
                 {`Jogar ${name}`}
-              </button>
+              </Button>
             </form>
           </Widget.Content>
         </Widget>
